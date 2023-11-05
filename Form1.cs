6b421064e70875148c018c903e70d278ab42a685
XI0N9MA0 <boeursamnang11@gmail.com>
2023-11-06 00:02:16 +0700

    new commit

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Utilities.BunifuCircleProgress.Transitions;
+
+namespace ecommercetestttt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Panha p = new Panha();
+            p.Pid = 0;
+            p.PidStr = "Panha";
+
+            MessageBox.Show("name:"+p.PidStr);
+        }
+    }
+}
