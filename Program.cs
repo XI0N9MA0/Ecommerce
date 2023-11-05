126b351e90a3fb270f9461934b91df8fc90353ad
XI0N9MA0 <boeursamnang11@gmail.com>
2023-11-06 00:23:38 +0700

    changed

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace ecommercetestttt
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormLogin());
+            Application.Run(new FormSignup());
         }
     }
 }
