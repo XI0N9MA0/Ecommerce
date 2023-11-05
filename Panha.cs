1d1b9d2474a4a7929f24bd7426306310862f2607
XI0N9MA0 <boeursamnang11@gmail.com>
2023-11-05 23:44:59 +0700

    add new

---
+++ Panha.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ecommercetestttt
+{
+    class Panha
+    {
+        int id;
+        public int Id { get { return id;} }
+        string name;
+        public string Name { get { return name;} }
+
+        public int Pid { get { return id;} set { id = value;} }
+        public string PidStr { get {  return name; } set { name = value; } }
+    }
+}
